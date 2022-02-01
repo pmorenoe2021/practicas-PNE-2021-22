20121eaf8339b1dbe416854adb81476c381a3db8
pmorenoe2021 <p.morenoe.2021@alumnos.urjc.es>
2022-02-01 16:02:21 +0100

    this commit shows info related to the file or specific function. for example: creation of calculate weight

--- hello/prueba.py
+++ hello/prueba.py
@@ -1,2 +1,3 @@
 print("this is just a test for us to know how it works")
 print("i dont need to save again, it does it automatically, you have to commit and push again")
+# always commit and push, if you don´t push it will not be uploaded to git
