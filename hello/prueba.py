cdc8aed5bd57b661e6042b040c8653f12f6da06e
pmorenoe2021 <p.morenoe.2021@alumnos.urjc.es>
2022-02-01 16:23:47 +0100

    this commit shows info related to the file or specific function. for example: creation of calculate weight

--- hello/prueba.py
+++ hello/prueba.py
@@ -1,3 +1,16 @@
 print("this is just a test for us to know how it works")
 print("i dont need to save again, it does it automatically, you have to commit and push again")
 # always commit and push, if you don´t push it will not be uploaded to git
+# puedes ejecutarlo con el play, en este caso cuidado con lo que esta ejecutando mira que lo de la izquierda
+# corresponda con lo que quieres ejecutar
+# o dandole al boton derecho encima del file y le das a run "nombre file
+# tabien puedes hacer:
+# cd..
+# cd..
+# python.exe .\prueba.py
+
+#how to set a breakpoint, press next to the number of the lines
+# si no te imprime nada dale al boton debugger, dale a console
+# y al segundo emoticono que te imprime una linea y pasa a la siguiente
+# you can set different breakpoints in a file
+# if we press resume in the debug console the program will be executed until the next breakpoint
