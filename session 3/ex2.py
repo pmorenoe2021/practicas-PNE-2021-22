8cee98c942c1f94108066d8931c05408f9a6e392
pmorenoe2021 <p.morenoe.2021@alumnos.urjc.es>
2022-02-02 17:56:26 +0100

    this commit shows info related to the file or specific function. for example: creation of calculate weight

---
+++ session 3/ex2.py
@@ -0,0 +1,20 @@
+#EXCERCISE 2: Convert the previous program into the function fibon(n) that calculates the nth Fibonacci term and return it
+# The main program should call the fibon() function and print the 5th, 10th and 15th terms in the console
+def fib(n):
+    n1 = 0
+    n2 = 1
+    if n == 1:
+        return n1
+    elif n == 2:
+        return n2
+    else:
+        for i in range(2, n + 1):
+            num = n1 + n2
+            n1 = n2
+            n2 = num
+        return num
+print("5th FIBONACCIS TERM",fib(5))
+print("11th FIBONACCIS TERM",fib(11))
+print("55h FIBONACCIS TERM",fib(55))
+#NEVER WRITE THE REUTURN INSIDE A LOOP
+
