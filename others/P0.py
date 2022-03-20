b9d15442643a402dbb5b152f36f47b02e0ad7076
pmorenoe2021 <p.morenoe.2021@alumnos.urjc.es>
2022-03-20 13:38:59 +0100

    this commit shows info related to the file or specific function. for example: creation of calculate weight

---
+++ others/P0.py
@@ -0,0 +1,86 @@
+def seq_ping():
+    print("Testing the seq_ping() funcion")
+    return "OK"
+
+
+def seq_len(seq):
+    return len(seq)
+
+
+def seq_read_fasta(filename):  # 3
+    from pathlib import Path
+    file_contents = Path(filename).read_text()
+    lines = file_contents.splitlines()
+    body = lines[1:]
+    sequence = ""
+    for line in body:
+        sequence += line
+    return sequence
+
+
+def seq_count_base(seq, base):
+    base = ["A", "C", "T", "G"]
+    ca = 0
+    cc = 0
+    ct = 0
+    cg = 0
+    for e in seq:
+        if e == "A":
+            ca += 1
+        elif e == "C":
+            cc += 1
+        elif e == "T":
+            ct += 1
+        elif e == "G":
+            cg += 1
+
+    return ca, cc, ct, cg
+
+
+def seq_count_base(seq, base):
+    return seq.count(base)
+    # OTRA MANERA MAS ESPARTANA
+    # total = 0
+    # for b in seq:
+    #    if b == base:
+    #        total += 1
+    # return total
+
+
+def seq_count(seq):
+    d = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    for e in seq:
+        if e == "A":
+            d["A"] += 1
+        elif e == "T":
+            d["T"] += 1
+        elif e == "C":
+            d["C"] += 1
+        elif e == "G":
+            d["G"] += 1
+    return d
+
+
+def seq_reverse(seq):
+    frag = seq[:20]
+    rev = frag[::-1]
+    return frag, rev
+
+
+def seq_complement(seq):
+    COMPLEMENTS = {"A": "T", "C": "G", "T": "A", "G": "C"}
+    result = ""
+    for base in seq:
+        result += COMPLEMENTS[base]
+    return result
+
+
+def most_f_b(seq):
+    d = seq_count(seq)
+    max_k = None
+    max_v = 0
+    for k, v in d.items(): # podrias haber puesto seq_count(seq).items(
+        if v >= max_v:
+            max_v = v
+            max_k = k
+    return max_k
---
+++ others/e0.py
@@ -0,0 +1,90 @@
+import P0
+print("-----| Exercise 1 |------")
+f = P0.seq_ping()
+print(f)
+print( )
+
+
+
+print("-----| Exercise 2 |------")
+FOLDER = "./sequences/"
+filename = input("File´s name: ")
+print(f"DNA file: {filename}")
+sequence = P0.seq_read_fasta(FOLDER + filename)
+print(f"The first 20 bases are: {sequence[:20]}")
+print()
+
+
+
+print("-----| Exercise 3 |------")
+FOLDER = "./sequences/"
+l = ["U5", "ADA", "FXN", "FRAT1"]
+
+for seq in l:
+    filename = seq + ".txt"
+    sequence = P0.seq_read_fasta(FOLDER + filename)
+    print(f"Gene {seq} ---> Length: {P0.seq_len(sequence)}")
+
+
+print("-----| Exercise 4 |------")
+FOLDER = "./sequences/"
+l = ["U5", "ADA", "FXN", "FRAT1"]
+bases = ["A", "C", "T", "G"]
+
+for seq in l:
+    filename = seq + ".txt"
+    sequence = P0.seq_read_fasta(FOLDER + filename)
+    print(f"Gene {seq}")
+    for base in bases:
+        print(f"{base} : {P0.seq_count_base(sequence, base)}")
+
+
+print()
+
+
+print("-----| Exercise 5 |------")
+FOLDER = "./sequences/"
+l = ["U5", "ADA", "FXN", "FRAT1"]
+for seq in l:
+    filename = seq + ".txt"
+    sequence = P0.seq_read_fasta(FOLDER + filename)
+    print(f"Gene {seq}: {P0.seq_count(sequence)}")
+print()
+
+
+
+print("-----| Exercise 6 |------")
+seq = "U5"
+FOLDER = "./sequences/"
+filename = seq + ".txt"
+sequence = P0.seq_read_fasta(FOLDER + filename)
+
+frag, rev = P0.seq_reverse(sequence)
+print(f"Frag: {frag}\nRev: {rev}")
+print()
+
+
+
+print("-----| Exercise 7 |------")
+gene = "U5"
+FOLDER = "./sequences/"
+filename = gene + ".txt"
+
+sequence = P0.seq_read_fasta(FOLDER + filename)
+print(f"Gene: {gene}")
+frag = sequence[:20]
+print(f"Frag: {frag}\nComp: {P0.seq_complement(frag)}")
+print()
+
+
+
+print("-----| Exercise 8 |------")
+FOLDER = "./sequences/"
+genes = ["U5", "ADA", "FXN", "FRAT1"]
+
+for seq in genes:
+    filename = seq + ".txt"
+    sequence = P0.seq_read_fasta(FOLDER + filename)
+    print(f"Gene {seq}: Most frequent Base: {P0.most_f_b(sequence)}")
+
+
